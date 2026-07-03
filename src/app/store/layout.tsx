792a18efc4245e87e84a0b2ec0dffd1f1ee8b77c
GEM CYBERSECURITY-MONITORING ASSIST <Analyzer@gemcybersecurityassist.com>
2026-07-03 11:18:24 +0000

    Update store layout navigation

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -2,10 +2,5 @@ import type { ReactNode } from "react";
 import { StorefrontNavigation } from "@/components/store/StorefrontNavigation";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <StorefrontNavigation active="all" />
-      {children}
-    </>
-  );
+  return <><StorefrontNavigation />{children}</>;
 }
