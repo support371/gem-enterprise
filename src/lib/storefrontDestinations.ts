4288bf63570c98df61819b781782d52012160738
GEM CYBERSECURITY-MONITORING ASSIST <Analyzer@gemcybersecurityassist.com>
2026-07-06 11:14:21 +0100

    feat(store): centralize connected storefront destinations

---
+++ src/lib/storefrontDestinations.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_MAIN_STORE_URL =
+  "https://crystal-kinetic-cart-flow.base44.app/";
+
+export const TIKTOK_MAIN_STORE_URL =
+  "https://superagent-17ea9ea1.base44.app/functions/tiktokStore";
