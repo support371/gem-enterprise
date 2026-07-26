420e3ee190f63feed7f380b8d12ba2c4a7d04e22
GEM CYBERSECURITY-MONITORING ASSIST <Analyzer@gemcybersecurityassist.com>
2026-07-26 06:45:23 +0100

    feat(video): export automated worker production runtime

--- src/lib/video/worker-runtime/index.ts
+++ src/lib/video/worker-runtime/index.ts
@@ -1,7 +1,38 @@
 export { loadVideoWorkerConfig, redactedWorkerConfig } from "./config";
+export { processVideoWorkerDispatchJob } from "./dispatch";
 export { downloadVideoOutput, uploadDownloadedVideo } from "./io";
-export { checkVideoWorkerReadiness, fetchWorkerJobs, verifyUploadedVideo } from "./network";
-export { buildComfyOutputUrl, buildStorageObjectPath, buildStorageObjectUrl, computeBackoffMs, mimeTypeForFileName, sanitizePathSegment, selectVideoOutput } from "./outputs";
+export {
+  deleteDispatchJournal,
+  ensureWorkerStateDirectory,
+  readDispatchJournal,
+  writeDispatchJournal,
+} from "./journal";
+export {
+  checkVideoWorkerReadiness,
+  claimWorkerDispatchJobs,
+  completeWorkerDispatch,
+  failWorkerDispatch,
+  fetchWorkerJobs,
+  finalizeVerifiedRenders,
+  verifyUploadedVideo,
+} from "./network";
+export {
+  buildComfyOutputUrl,
+  buildStorageObjectPath,
+  buildStorageObjectUrl,
+  computeBackoffMs,
+  mimeTypeForFileName,
+  sanitizePathSegment,
+  selectVideoOutput,
+} from "./outputs";
 export { processVideoWorkerJob } from "./processor";
 export { VideoWorkerError } from "./types";
-export type { DownloadedVideo, VideoOutputDescriptor, VideoWorkerConfig, VideoWorkerEnvironment, VideoWorkerJob } from "./types";
+export type {
+  DownloadedVideo,
+  VideoOutputDescriptor,
+  VideoWorkerConfig,
+  VideoWorkerDispatchJob,
+  VideoWorkerDispatchJournal,
+  VideoWorkerEnvironment,
+  VideoWorkerJob,
+} from "./types";
